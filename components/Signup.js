5c014f58b021bca8c4c9f3bafa1a86e5cc27495c
raphaelle <raflanelle@hotmail.com>
2024-07-18 17:26:30 +0200

    frontend avec signup fonctionnel avec link

--- components/Signup.js
+++ components/Signup.js
@@ -37,7 +37,7 @@ function Signup({isOpen, onClose, children}) {
                     <input className={styles.name} type="text" placeholder="Name" id="signUpName" onChange={(e) => setSignUpName(e.target.value)} value={signUpName} />
 					<input className={styles.name} type="text" placeholder="Username" id="signUpUsername" onChange={(e) => setSignUpUsername(e.target.value)} value={signUpUsername} />
 					<input className={styles.name} type="password" placeholder="Password" id="signUpPassword" onChange={(e) => setSignUpPassword(e.target.value)} value={signUpPassword} />
-					<Link href='/Home.js'><button className={styles.sign} id="register" onClick={() => handleRegister()}>Sign-up</button></Link>
+					<Link href='/'><button className={styles.sign} id="register" onClick={() => handleRegister()}>Sign-up</button></Link>
 				</div>
     </div>
      
