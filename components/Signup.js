1e7754b25821a3dafa8608f2f58889836e0d88e1
raphaelle <raflanelle@hotmail.com>
2024-07-18 17:20:05 +0200

    frontend avec signup fonctionnelet link

--- components/Signup.js
+++ components/Signup.js
@@ -2,7 +2,7 @@ import styles from '../styles/Signup.module.css';
 import {useState} from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { addUser } from '../reducers/user';
-
+import Link from 'next/Link';
 
 function Signup({isOpen, onClose, children}) {
 
@@ -32,11 +32,12 @@ function Signup({isOpen, onClose, children}) {
     <div className='Signin'>
     <div className={styles.registerSection}>
                     <img src="/logo.svg" alt="logo" className={styles.logo} />
+                    <button className={styles.overlay_close} type='button' onClick={onClose}>x</button>
 					<p className={styles.create}>Create your Hackatweet account</p>
                     <input className={styles.name} type="text" placeholder="Name" id="signUpName" onChange={(e) => setSignUpName(e.target.value)} value={signUpName} />
 					<input className={styles.name} type="text" placeholder="Username" id="signUpUsername" onChange={(e) => setSignUpUsername(e.target.value)} value={signUpUsername} />
 					<input className={styles.name} type="password" placeholder="Password" id="signUpPassword" onChange={(e) => setSignUpPassword(e.target.value)} value={signUpPassword} />
-					<button className={styles.sign} id="register" onClick={() => handleRegister()}>Sign-up</button>
+					<Link href='/Home.js'><button className={styles.sign} id="register" onClick={() => handleRegister()}>Sign-up</button></Link>
 				</div>
     </div>
      
