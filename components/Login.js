48dfef1500dbfd8e8092a8bb501071279561ea93
raphaelle <raflanelle@hotmail.com>
2024-07-19 06:54:38 +0200

    frontend page login avec signup et signin fonctionnel + overlay

--- components/Login.js
+++ components/Login.js
@@ -5,33 +5,30 @@ import Signin from './Signin';
 
 
 function Login() {
-   
 
-    const [isSignupOpen, setIsSignupOpen] = useState(false); 
-    
-    
+
+    const [isSignupOpen, setIsSignupOpen] = useState(false);
+    const [isSigninOpen, setIsSigninOpen] = useState(false);
+    console.log(`signin ${isSigninOpen}`)
+    console.log(`signup ${isSignupOpen}`)
+
 
     return (
         <div className={styles.login}>
-           <img src="/image_twitter.png" alt="twitter" className={styles.background}/>
-           <div className={styles.container}>
-           <img src="logo.svg" alt="logo" className={styles.logo} />
-           <h1 className={styles.title}>See what's happening</h1>
-           <h3 className={styles.join}>Join Hackatweet today</h3>
-           <button className={styles.signup} id="Signup" onClick={() => setIsSignupOpen(!isSignupOpen)}>Sign up</button>
-           <h4>already have an account?</h4>
-           <button className={styles.signin} id="signin" onClick={() => handleSignin()}>Sign in</button>
-           </div>
-           <Signup/>
+            <img src="/image_twitter.png" alt="twitter" className={styles.background} />
+            <div className={styles.container}>
+                <img src="logo.svg" alt="logo" className={styles.logo} />
+                <h1 className={styles.title}>See what's happening</h1>
+                <h3 className={styles.join}>Join Hackatweet today</h3>
+                <button className={styles.signup} id="Signup" onClick={() => setIsSignupOpen(!isSignupOpen)}>Sign up</button>
+                <h4>already have an account?</h4>
+                <button className={styles.signin} id="Signin" onClick={() => setIsSigninOpen(!isSigninOpen)}>Sign in</button>
+            </div>
+            <Signup isOpen={isSignupOpen} onClose={() => setIsSignupOpen(!isSignupOpen)} />
+            <Signin isOpen={isSigninOpen} onClose={() => setIsSigninOpen(!isSigninOpen)} />
         </div>
     );
 }
 
 export default Login;
 
-
- {/*const [isOverlayOpen, setIsOverlayOpen] = useState(false); 
-    <button onClick={() => setIsOverlayOpen(!isOverlayOpen)}>Open Overlay</button>
-            <Login isOpen={isOverlayOpen} onClose={() => setIsOverlayOpen(!isOverlayOpen)} />
-            <h1> hello from overlay</h1> */}
-            
